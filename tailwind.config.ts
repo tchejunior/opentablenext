4a5e744b16587b741b69ede26c5335b3b63e5a9b
Marcelo Brigato <marcelo.brigato_c@tdmsystems.com>
2024-01-15 22:58:09 -0300

    Lesson 19

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
+    },
+    fontSize: {
+      '2xsm': '10px',
+      xsm: '12px',
+      sm: '13px',
+      reg: '15px',
+      lg: '18px',
+      '2xl': '22px',
+      '3xl': '25px',
+      '4xl': '32px',
+      '5xl': '40px',
+      '6xl': '50px',
+      '7xl': '70px'
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
