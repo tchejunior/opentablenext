5fba9963896064193c8c3c7a3ec6c8b53b305467
Marcelo Brigato <marcelo.brigato_c@tdmsystems.com>
2024-03-02 17:26:34 -0300

    Lesson 62 Auth modal fields

--- app/restaurant/error.tsx
+++ app/restaurant/error.tsx
@@ -4,7 +4,7 @@ import errorIcon from '../../public/icons/error.png';
 
 import Image from 'next/image';
 
-export default function NotFound({ error }: { error: Error }) {
+export default function Error({ error }: { error: Error }) {
   return (
     <div className='h-screen bg-gray-200 flex flex-col justify-center items-center'>
       <Image src={errorIcon} alt='error' className='w-56 mb-8' />
--- app/restaurant/not-found.tsx
+++ app/restaurant/not-found.tsx
@@ -4,7 +4,7 @@ import errorIcon from '../../public/icons/error.png';
 
 import Image from 'next/image';
 
-export default function error() {
+export default function NotFound() {
   return (
     <div className='h-screen bg-gray-200 flex flex-col justify-center items-center'>
       <Image src={errorIcon} alt='error' className='w-56 mb-8' />
