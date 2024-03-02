6ae0d5c302ad8c9418e9e5ca0d9eaca883adb53b
Marcelo Brigato <marcelo.brigato_c@tdmsystems.com>
2024-03-02 11:00:09 -0300

    Lesson 56 Overriding default loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -9,13 +9,7 @@ export default function loading() {
           <div
             key={i}
             className='animate-pulse bg-slate-200 w-64 h-72 rounded overflow-hidden border cursor-pointer m-3'
-          >
-            {/* <div className='w-full h-3/4 bg-gray-300 rounded-t-lg'></div>
-            <div className='flex justify-between items-center p-4'>
-              <div className='w-2/3 h-6 bg-gray-300 rounded-lg'></div>
-              <div className='w-1/3 h-6 bg-gray-300 rounded-lg'></div>
-            </div> */}
-          </div>
+          ></div>
         ))}
       </div>
     </main>
