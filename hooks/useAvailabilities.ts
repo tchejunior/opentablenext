8dc5c178b8721434604845f4d1236c222a9591be
Marcelo Brigato <marcelo.brigato_c@tdmsystems.com>
2024-03-04 23:07:24 -0300

    Lesson 103 - Add useAvailabilities custom hook

---
+++ hooks/useAvailabilities.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+export default function useAvailabilities() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  const fetchAvailabilities = async ({
+    slug,
+    day,
+    time,
+    partySize,
+  }: {
+    slug: string;
+    day: string;
+    time: string;
+    partySize: string;
+  }) => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get(`/api/restaurant/${slug}/availability`, {
+        params: {
+          day,
+          time,
+          partySize,
+        },
+      });
+      setLoading(false);
+      setData(response.data);
+    } catch (error: any) {
+      setLoading(false);
+      setError(error.response.data.errorMessage);
+    }
+  };
+
+  return { loading, error, data, fetchAvailabilities };
+}
